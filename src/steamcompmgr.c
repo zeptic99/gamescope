32fe2d5bfbfc349e5d55f8d1d76049b077b0127a
Pierre-Loup A. Griffais <git@plagman.net>
2019-11-03 16:53:18 -0800

    Register GL error callback instead of checking by hand.

--- src/steamcompmgr.c
+++ src/steamcompmgr.c
@@ -865,12 +865,6 @@ paint_all (Display *dpy)
 		paint_debug_info(dpy);
 	
 	waffle_window_swap_buffers(window);
-	
-	if (glGetError() != GL_NO_ERROR)
-	{
-		fprintf (stderr, "GL error!\n");
-		exit (1);
-	}
 }
 
 static void
@@ -1703,6 +1697,20 @@ void check_new_wayland_res(void)
 	}
 }
 
+void GLAPIENTRY
+MessageCallback( GLenum source,
+				 GLenum type,
+				 GLuint id,
+				 GLenum severity,
+				 GLsizei length,
+				 const GLchar* message,
+				 const void* userParam )
+{
+	fprintf( stderr, "GL ERROR: %s type = 0x%x, severity = 0x%x, message = %s\n",
+			 ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+			 type, severity, message );
+}
+
 int
 steamcompmgr_main (int argc, char **argv)
 {
@@ -1826,6 +1834,9 @@ steamcompmgr_main (int argc, char **argv)
 	__pointer_to_eglDestroyImageKHR = (PFNEGLDESTROYIMAGEKHRPROC)eglGetProcAddress("eglDestroyImageKHR");
 	
 	eglSwapInterval( eglGetCurrentDisplay(), 1 );
+	
+	glEnable (GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(MessageCallback, 0);
 	
 	glEnable(GL_TEXTURE_2D);
 	glGenTextures(1, &cursorTextureName);
