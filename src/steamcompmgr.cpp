zeptic99/gamescope

commit 4a5cd5d2bd07cf6e5586f37bd9e93e17278b81bc

steamcompmgr: disable Steam overlay for sub-command
If running through Steam, we want the overlay hooked into gamescope and
not the underlying game, or we'll get double overlay, and one with wrong
scaling.

This strips any token containing gameoverlayrenderer.so from LD_PRELOAD.

src/steamcompmgr.cpp
@@ -2370,7 +2370,74 @@ steamcompmgr_main (int argc, char **argv)
 		// (Don't Lose) The Children
 		prctl( PR_SET_CHILD_SUBREAPER );
 
-		posix_spawnp( &pid, argv[ g_nSubCommandArg ], NULL, &attr, &argv[ g_nSubCommandArg ], environ );
+		std::vector< char * > vecNewEnviron;
+		char ** oldEnviron = environ;
+
+		while ( *oldEnviron != nullptr )
+		{
+			size_t envLength = strlen( *oldEnviron ) + 1;
+			char *envVar = new char[ envLength ];
+			bool bFirst = true;
+
+			strcpy( envVar, *oldEnviron );
+
+			if ( strstr( envVar, "LD_PRELOAD=" ) == envVar )
+			{
+				std::string strNewPreload = "LD_PRELOAD=";
+
+				// First replace all the separators in our temporary copy with terminators
+				for ( char *c = &envVar[ 11 ]; c < envVar + envLength; c++ )
+				{
+					if ( *c == ' ' || *c == ':' )
+					{
+						*c = '\0';
+					}
+				}
+
+				// Then walk it again and find all the substrings
+				for ( char *c = &envVar[ 11 ]; c < envVar + envLength; c++ )
+				{
+					// If there's a string and it's not gameoverlayrenderer, append it to our new LD_PRELOAD
+					if ( *c != '\0' )
+					{
+						if ( strstr( c, "gameoverlayrenderer.so" ) == nullptr )
+						{
+							if ( bFirst == false )
+							{
+								strNewPreload.append( ":" );
+							}
+							else
+							{
+								bFirst = false;
+							}
+
+							strNewPreload.append( c );
+						}
+
+						c += strlen ( c );
+					}
+				}
+
+				// Then copy back to our allocated string, it has to be strictly larger than the original
+				strcpy( envVar, strNewPreload.c_str() );
+			}
+
+			vecNewEnviron.push_back( envVar );
+
+			oldEnviron++;
+		}
+
+		vecNewEnviron.push_back( nullptr ) ;
+
+		posix_spawnp( &pid, argv[ g_nSubCommandArg ], NULL, &attr, &argv[ g_nSubCommandArg ], vecNewEnviron.data() );
+
+		for ( uint32_t i = 0; i < vecNewEnviron.size(); i++ )
+		{
+			if ( vecNewEnviron[ i ] != nullptr )
+			{
+				delete vecNewEnviron[ i ];
+			}
+		}
 
 		std::thread waitThread([](){
 			while( wait( nullptr ) >= 0 )

commit 3096c1e9cf602c8cb415d56502f04b5dbbecb4ce

Fix alloc-dealloc-mismatch in vecNewEnviron
    ==2654441==ERROR: AddressSanitizer: alloc-dealloc-mismatch (operator new [] vs operator delete) on 0x6020000c0330
        #0 0x7f264e95e009 in operator delete(void*, unsigned long) /build/gcc/src/gcc/libsanitizer/asan/asan_new_delete.cpp:172
        #1 0x5645ddfcef1e in steamcompmgr_main(int, char**) ../src/steamcompmgr.cpp:2602
    Running Steam on arch rolling 64-bit
        #2 0x5645de0020e6 in steamCompMgrThreadRun() ../src/main.cpp:126
        #3 0x5645de001a2f in void std::__invoke_impl<void, void (*)()>(std::__invoke_other, void (*&&)()) /usr/include/c++/10.2.0/bits/invoke.h:60
        #4 0x5645de001899 in std::__invoke_result<void (*)()>::type std::__invoke<void (*)()>(void (*&&)()) /usr/include/c++/10.2.0/bits/invoke.h:95
        #5 0x5645de001757 in void std::thread::_Invoker<std::tuple<void (*)()> >::_M_invoke<0ul>(std::_Index_tuple<0ul>) /usr/include/c++/10.2.0/thread:264
        #6 0x5645de00161a in std::thread::_Invoker<std::tuple<void (*)()> >::operator()() /usr/include/c++/10.2.0/thread:271
        #7 0x5645de000d98 in std::thread::_State_impl<std::thread::_Invoker<std::tuple<void (*)()> > >::_M_run() /usr/include/c++/10.2.0/thread:215
    STEAM_RUNTIME is enabled automatically
        #8 0x7f264e143c23 in execute_native_thread_routine /build/gcc/src/gcc/libstdc++-v3/src/c++11/thread.cc:80
        #9 0x7f264d58b3e8 in start_thread (/usr/lib/libpthread.so.0+0x93e8)
        #10 0x7f264d4b9292 in __GI___clone (/usr/lib/libc.so.6+0x100292)

Fixes: 4a5cd5d2bd07 ("steamcompmgr: disable Steam overlay for sub-command")

src/steamcompmgr.cpp
@@ -2452,7 +2452,7 @@ steamcompmgr_main (int argc, char **argv)
 		{
 			if ( vecNewEnviron[ i ] != nullptr )
 			{
-				delete vecNewEnviron[ i ];
+				delete[] vecNewEnviron[ i ];
 			}
 		}
 