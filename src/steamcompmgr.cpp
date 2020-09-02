d9920c90876e129b0d12ace10703c7c8ecf254aa
Joshua Ashton <joshua@froggi.es>
2020-09-02 09:38:47 +0100

    Add to-do note for frame submission time

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -2932,6 +2932,9 @@ steamcompmgr_main (int argc, char **argv)
 				hasRepaint = false;
 			}
 
+			// TODO: Look into making this _RAW
+			// wlroots, seems to just use normal MONOTONIC
+			// all over so this may be problematic to just change.
 			struct timespec now;
 			clock_gettime(CLOCK_MONOTONIC, &now);
 
