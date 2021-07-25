zeptic99/gamescope

commit 8b7063799874fc5f1968cfdd985722357aba3d5d

Use VK_FORMAT_B8G8R8A8_UNORM no swizzle for DRM_FORMAT_ARGB8888
We use this as an input not as the output so we can just use the DRM format as this exactly with no swizzle.

This format is guaranteed to be supported.

src/rendervulkan.cpp
@@ -147,8 +147,8 @@ struct {
 	bool bNeedsSwizzle;
 	bool bHasAlpha;
 } s_DRMVKFormatTable[] = {
-	{ DRM_FORMAT_XRGB8888, VK_FORMAT_R8G8B8A8_UNORM, true, false },
-	{ DRM_FORMAT_ARGB8888, VK_FORMAT_R8G8B8A8_UNORM, true, true },
+	{ DRM_FORMAT_XRGB8888, VK_FORMAT_B8G8R8A8_UNORM, false, false },
+	{ DRM_FORMAT_ARGB8888, VK_FORMAT_B8G8R8A8_UNORM, false, true },
 	{ DRM_FORMAT_NV12, VK_FORMAT_G8_B8R8_2PLANE_420_UNORM, false, false },
 	{ DRM_FORMAT_INVALID, VK_FORMAT_UNDEFINED, false, false },
 };

commit c9e8634e13560850e1227b6b939e636b1d56828a

rendervulkan: add back VK_FORMAT_R8G8B8A8_UNORM
This format is used for the cursor. Regressed by 8b7063799874 ("Use
VK_FORMAT_B8G8R8A8_UNORM no swizzle for DRM_FORMAT_ARGB8888").

    gamescope: ../src/rendervulkan.cpp:369: bool CVulkanTexture::BInit(uint32_t, uint32_t, VkFormat, CVulkanTexture::createFlags, wlr_dmabuf_attributes*): Assertion drmFormat != DRM_FORMAT_INVALID' failed.

src/rendervulkan.cpp
@@ -149,6 +149,8 @@ struct {
 } s_DRMVKFormatTable[] = {
 	{ DRM_FORMAT_XRGB8888, VK_FORMAT_B8G8R8A8_UNORM, false, false },
 	{ DRM_FORMAT_ARGB8888, VK_FORMAT_B8G8R8A8_UNORM, false, true },
+	{ DRM_FORMAT_XRGB8888, VK_FORMAT_R8G8B8A8_UNORM, true, false },
+	{ DRM_FORMAT_ARGB8888, VK_FORMAT_R8G8B8A8_UNORM, true, true },
 	{ DRM_FORMAT_NV12, VK_FORMAT_G8_B8R8_2PLANE_420_UNORM, false, false },
 	{ DRM_FORMAT_INVALID, VK_FORMAT_UNDEFINED, false, false },
 };