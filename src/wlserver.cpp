zeptic99/gamescope

commit 7c0f65e62dc88f1b29aea235e57c09e0a81bf262

wlserver: stop re-creating wl_output global

src/wlserver.cpp
@@ -627,9 +627,7 @@ static void handle_wlr_log(enum wlr_log_importance importance, const char *fmt, 
 
 void wlserver_set_output_info( const wlserver_output_info *info )
 {
-	free(wlserver.output_info.name);
 	free(wlserver.output_info.description);
-	wlserver.output_info.name = strdup(info->name);
 	wlserver.output_info.description = strdup(info->description);
 	wlserver.output_info.phys_width = info->phys_width;
 	wlserver.output_info.phys_height = info->phys_height;
@@ -682,7 +680,6 @@ bool wlsession_init( void ) {
 	wl_display_set_global_filter(wlserver.display, filter_global, nullptr);
 
 	const struct wlserver_output_info output_info = {
-		.name = "Virtual-1",
 		.description = "Virtual gamescope output",
 	};
 	wlserver_set_output_info( &output_info );
@@ -760,6 +757,7 @@ gamescope_xwayland_server_t::gamescope_xwayland_server_t(wl_display *display)
 	output = wlr_headless_add_output(wlserver.wlr.headless_backend, 1280, 720);
 	strncpy(output->make, "gamescope", sizeof(output->make));
 	strncpy(output->model, "gamescope", sizeof(output->model));
+	wlr_output_set_name(output, "gamescope");
 
 	int refresh = g_nNestedRefresh;
 	if (refresh == 0) {
@@ -787,14 +785,17 @@ void gamescope_xwayland_server_t::update_output_info()
 {
 	const auto *info = &wlserver.output_info;
 
-	wlr_output_destroy_global(output);
-
-	wlr_output_set_name(output, info->name);
-	wlr_output_set_description(output, info->description);
 	output->phys_width = info->phys_width;
 	output->phys_height = info->phys_height;
+	struct wl_resource *resource;
+	wl_resource_for_each(resource, &output->resources) {
+		wl_output_send_geometry(resource, 0, 0,
+			output->phys_width, output->phys_height, output->subpixel,
+			output->make, output->model, output->transform);
+	}
+	wlr_output_schedule_done(output);
 
-	wlr_output_create_global(output);
+	wlr_output_set_description(output, info->description);
 }
 
 bool wlserver_init( void ) {

src/drm.cpp
@@ -760,7 +760,6 @@ static bool setup_best_connector(struct drm_t *drm, bool force)
 	}
 
 	const struct wlserver_output_info wlserver_output_info = {
-		.name = best->name,
 		.description = description,
 		.phys_width = (int) best->connector->mmWidth,
 		.phys_height = (int) best->connector->mmHeight,

src/wlserver.hpp
@@ -171,7 +171,6 @@ void wlserver_send_frame_done( struct wlr_surface *surf, const struct timespec *
 bool wlserver_surface_is_async( struct wlr_surface *surf );
 
 struct wlserver_output_info {
-	const char *name;
 	const char *description;
 	int phys_width, phys_height; // millimeters
 };

commit 79ad108c41c3a804664133c089a3ae12fecf1cb2

wlserver: fix output global not exposed
Fixes: 7c0f65e62dc8 ("wlserver: stop re-creating wl_output global")

src/wlserver.cpp
@@ -773,6 +773,8 @@ gamescope_xwayland_server_t::gamescope_xwayland_server_t(wl_display *display)
 	}
 
 	update_output_info();
+
+	wlr_output_create_global(output);
 }
 
 gamescope_xwayland_server_t::~gamescope_xwayland_server_t()