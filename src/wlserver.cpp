f0fd4e6301b71e21a8e7d627f4b927b9cc1e0677
Joshua Ashton <joshua@froggi.es>
2023-03-16 23:23:00 +0000

    wlserver: Be more robust when changing override windows

--- src/wlserver.cpp
+++ src/wlserver.cpp
@@ -1569,10 +1569,24 @@ static void wlserver_x11_surface_info_set_wlr( struct wlserver_x11_surface_info 
 	wlserver_wl_surface_info *wl_surf_info = get_wl_surface_info(wlr_surf);
 	if (override)
 	{
+		if ( surf->override_surface )
+		{
+			wlserver_wl_surface_info *wl_info = get_wl_surface_info(surf->override_surface);
+			if (wl_info)
+				wl_info->x11_surface = nullptr;
+		}
+
 		surf->override_surface = wlr_surf;
 	}
 	else
 	{
+		if ( surf->main_surface )
+		{
+			wlserver_wl_surface_info *wl_info = get_wl_surface_info(surf->main_surface);
+			if (wl_info)
+				wl_info->x11_surface = nullptr;
+		}
+
 		surf->main_surface = wlr_surf;
 	}
 	wl_surf_info->x11_surface = surf;
@@ -1600,6 +1614,7 @@ static void wlserver_x11_surface_info_set_wlr( struct wlserver_x11_surface_info 
 
 void wlserver_x11_surface_info_init( struct wlserver_x11_surface_info *surf, gamescope_xwayland_server_t *server, uint32_t x11_id )
 {
+	fprintf( stderr, "wlserver_x11_surface_info_init: %p\n", surf );
 	surf->wl_id = 0;
 	surf->x11_id = x11_id;
 	surf->main_surface = nullptr;
@@ -1658,6 +1673,7 @@ const char *gamescope_xwayland_server_t::get_nested_display_name() const
 void wlserver_x11_surface_info_finish( struct wlserver_x11_surface_info *surf )
 {
 	assert( wlserver_is_lock_held() );
+	fprintf( stderr, "wlserver_x11_surface_info_finish: %p (%p %p)\n", surf, surf->main_surface.load(), surf->override_surface.load() );
 
 	if (surf->main_surface)
 	{
