a8ffc64f40c740b25fb6a226cdaafe5edf215e3f
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-05-11 14:49:59 +0200

    Ensure that duplicated commits have forward progress.
    If the same buffer is attached in subsequent commits,
    ensure frame callback is pumped.
    This matches Mutter and Weston behavior based on tests.

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -5682,6 +5682,11 @@ void update_wayland_res(CommitDoneList_t *doneCommits, steamcompmgr_win_t *w, Re
 		wlr_buffer_unlock( buf );
 		wlserver_unlock();
 		xwm_log.errorf( "got the same buffer commited twice, ignoring." );
+
+		// If we have a duplicated commit + frame callback, ensure that is signalled.
+		// This matches Mutter and Weston behavior, so it's plausible that some application relies on forward progress.
+		// We're essentially discarding the commit here, so consider it complete right away.
+		w->receivedDoneCommit = true;
 		return;
 	}
 
