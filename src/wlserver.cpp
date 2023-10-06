7f067de65219bec8a1719c8a0a338cf27c4452b6
Joshua Ashton <joshua@froggi.es>
2023-10-06 23:39:36 +0100

    wlserver: Add feature bit for pixel filter

--- src/wlserver.cpp
+++ src/wlserver.cpp
@@ -894,6 +894,7 @@ static void gamescope_control_bind( struct wl_client *client, void *data, uint32
 	// Send feature support
 	gamescope_control_send_feature_support( resource, GAMESCOPE_CONTROL_FEATURE_RESHADE_SHADERS, 1, 0 );
 	gamescope_control_send_feature_support( resource, GAMESCOPE_CONTROL_FEATURE_DISPLAY_INFO, 1, 0 );
+	gamescope_control_send_feature_support( resource, GAMESCOPE_CONTROL_FEATURE_PIXEL_FILTER, 1, 0 );
 	gamescope_control_send_feature_support( resource, GAMESCOPE_CONTROL_FEATURE_DONE, 0, 0 );
 
 	wlserver.gamescope_controls.push_back(resource);
