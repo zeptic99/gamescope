2e1dd0b8e21c95b9895a622af52fe16bf78038c7
Joshua Ashton <joshua@froggi.es>
2023-12-06 23:21:02 +0000

    steamcompmgr: Move mwm hints to mwm_hints.h

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -88,6 +88,7 @@
 #include "log.hpp"
 #include "defer.hpp"
 #include "win32_styles.h"
+#include "mwm_hints.h"
 
 static const int g_nBaseCursorScale = 36;
 
@@ -822,34 +823,6 @@ static inline void GarbageCollectWaitableCommit( std::shared_ptr<commit_t> &comm
 }
 
 static std::vector<pollfd> pollfds;
-
-#define MWM_HINTS_FUNCTIONS   1
-#define MWM_HINTS_DECORATIONS 2
-#define MWM_HINTS_INPUT_MODE  4
-#define MWM_HINTS_STATUS      8
-
-#define MWM_FUNC_ALL          0x01
-#define MWM_FUNC_RESIZE       0x02
-#define MWM_FUNC_MOVE         0x04
-#define MWM_FUNC_MINIMIZE     0x08
-#define MWM_FUNC_MAXIMIZE     0x10
-#define MWM_FUNC_CLOSE        0x20
-
-#define MWM_DECOR_ALL         0x01
-#define MWM_DECOR_BORDER      0x02
-#define MWM_DECOR_RESIZEH     0x04
-#define MWM_DECOR_TITLE       0x08
-#define MWM_DECOR_MENU        0x10
-#define MWM_DECOR_MINIMIZE    0x20
-#define MWM_DECOR_MAXIMIZE    0x40
-
-#define MWM_INPUT_MODELESS                  0
-#define MWM_INPUT_PRIMARY_APPLICATION_MODAL 1
-#define MWM_INPUT_SYSTEM_MODAL              2
-#define MWM_INPUT_FULL_APPLICATION_MODAL    3
-#define MWM_INPUT_APPLICATION_MODAL         1
-
-#define MWM_TEAROFF_WINDOW 1
 
 Window x11_win(steamcompmgr_win_t *w) {
 	if (w == nullptr)
